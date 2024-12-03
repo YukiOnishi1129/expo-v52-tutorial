ff1157fd3ec9506a9707f9565e1f317c056f7ab2
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-12-03 17:20:01 +0900

    fix

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -9,6 +9,7 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
